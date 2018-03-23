607d3c91a9e6be010b58916d92ec15550ba2fb0c
dva <denisva@gmail.com>
2018-03-23 20:23:22 +0200

    HMR

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('test')
+import { inform } from './debug'
+
+if (module.hot) {
+  module.hot.dispose(function () {
+    inform('module is about to be replaced')
+  })
+
+  module.hot.accept(function () {
+    inform('module or one of its dependencies was just updated')
+  })
+}
