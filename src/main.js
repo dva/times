f026c0e3ced8cc7b34b1041e468f16b18f367d88
dva <denisva@gmail.com>
2020-05-05 22:55:10 +0300

    feat: main implementation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import './hmr'
 
-const times = Date.now()
+const start = new Date().getTime()
+let time = 0
+let elapsed = '0.0'
 
-document.getElementById('root').textContent = times
+function instance () {
+  time += 100
+
+  elapsed = Math.floor(time / 100) / 10
+  if (Math.round(elapsed) === elapsed) { elapsed += '.0' }
+
+  document.getElementById('root').textContent = elapsed
+  document.title = elapsed
+
+  const diff = (new Date().getTime() - start) - time
+  window.setTimeout(instance, (100 - diff))
+}
+
+window.setTimeout(instance, 100)
